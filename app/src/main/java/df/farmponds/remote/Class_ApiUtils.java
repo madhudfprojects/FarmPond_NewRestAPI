ac3dbc6b76f1b82f8d64a83088539f86924696a5
vijay <vijay.tech>
2020-08-03 17:12:55 +0530

    commit

--- app/src/main/java/df/farmponds/remote/Class_ApiUtils.java
+++ app/src/main/java/df/farmponds/remote/Class_ApiUtils.java
@@ -1,7 +1,10 @@
 package df.farmponds.remote;
 
 public class Class_ApiUtils {
-    public static final String BASE_URL = "https://agri.dfindia.org/api/";
+    //public static final String BASE_URL = "https://agri.dfindia.org/api/";
+
+    public static final String BASE_URL = "http://13.250.59.29/api/";
+    
 
     public static Interface_userservice getUserService() {
         return Class_RetrofitClient.getClient(BASE_URL).create(Interface_userservice.class);
