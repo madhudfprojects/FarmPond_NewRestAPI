7154567db82380438fe4e0fc2aa46aeb00872394
Admin <madhushree.kubsad@dfmail.org>
2020-08-25 11:00:15 +0530

    Madhu

--- app/src/main/java/df/farmponds/MainActivity.java
+++ app/src/main/java/df/farmponds/MainActivity.java
@@ -374,10 +374,10 @@ public class MainActivity extends AppCompatActivity
         //retrofit2.Call call = userService1.getValidateLoginPostNew("eventtest464@gmail.com");
        // retrofit2.Call call = userService1.getValidateLoginPostNew(str_gmailid);
        // str_gmailid="anandkanade.tech@dfmail.org";
-       // str_gmailid="eventtest464@gmail.com";
-       // str_gmailid="kanadeanand@gmail.com";
+        str_gmailid="eventtest464@gmail.com";
+      //  str_gmailid="kanadeanand@gmail.com";
       //  str_gmailid="testdev326@gmail.com";
-
+      //  str_gmailid="madhushree.kubsad@dfmail.org";
         retrofit2.Call call = userService1.getValidateLoginPostNew(str_gmailid);
 
         call.enqueue(new Callback()
